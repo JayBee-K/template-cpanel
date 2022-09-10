c190f89f9be5e179d505bda6b5fc3af22e3b3c00
JayBee-K <79182784+JayBee-K@users.noreply.github.com>
2022-09-10 10:19:59 +0700

    Update

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,58 @@
 	'use strict';
 	let windowWidth = $(window).width();
 
+	let initFromModule1 = function () {
+		let elmWrapper = $('#createRow');
+		$('#createRow').on('click', '.addRow', function () {
+			let theCaoSelect = $(this).closest('.row-item').find('#theCao'),
+				theCaoIndexSelected = theCaoSelect.find('option:selected').attr('data-index'),
+				theCaoRender = $(this).closest('.row-item').find('#theCao').clone();
+
+			theCaoRender.find('option').attr('selected', false);
+			theCaoRender.find('option[data-index=' + theCaoIndexSelected + ']').attr('selected', true);
+
+			let menhGiaSelect = $(this).closest('.row-item').find('#menhGia'),
+				menhGiaIndexSelected = menhGiaSelect.find('option:selected').attr('data-index'),
+				menhGiaRender = $(this).closest('.row-item').find('#menhGia').clone();
+
+			menhGiaRender.find('option').attr('selected', false);
+			menhGiaRender.find('option[data-index=' + menhGiaIndexSelected + ']').attr('selected', true);
+
+			let rowRender = `<div class="row-item row row5">
+                            <div class="col-lg-3 col-sm-12 col-12">
+							    ${theCaoRender[0].innerHTML}
+							 </div>
+							<div class="col-lg-3 col-sm-6 col-12">
+							    <div class="position-relative form-icon form-icon_right">
+								    <input type="text" class="form-control" placeholder="Mã thẻ">
+                                    <button type="button" class="copy-value form-button">
+                                        <i class="fas fa-paste"></i>
+                                    </button>
+								</div>
+							</div>
+							<div class="col-lg-3 col-sm-6 col-12">
+							    <div class="position-relative form-icon form-icon_right">
+								    <input type="text" class="form-control" placeholder="Mã seri">
+                                    <button type="button" class="copy-value form-button">
+                                        <i class="fas fa-paste"></i>
+                                    </button>
+								</div>
+							</div>
+							<div class="col-lg-2 col-sm-10 col-9">
+							    ${menhGiaRender[0].innerHTML}
+							</div>
+							<div class="col-lg-1 col-sm-2 col-3 text-right">
+								<button type="button" class="btn btn-small btn-danger deleteRow w-100">
+									<i class="fas fa-trash-alt"></i>
+								</button>
+							</div>
+						</div>`;
+			elmWrapper.append(rowRender);
+		}).on('click', '.deleteRow', function () {
+			$(this).closest('.row-item').remove();
+		});
+	}
+
 	let initFormFloating = function () {
 		if ($('.form-floating').length) {
 			$('.form-floating .form-control').blur(function () {
@@ -28,6 +80,7 @@
 	}
 
 	$(function () {
+		initFromModule1();
 		initFormFloating();
 		$(document).on('click', '.copy-value', function () {
 			if ($(this).attr('data-value') != undefined) {
