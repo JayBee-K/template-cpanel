cfc43ae991b9a1eac453492c647d8c19b58fa5f5
Jaybee-k <79182784+JayBee-K@users.noreply.github.com>
2022-09-13 22:41:15 +0700

    Update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,52 +1,61 @@
 ;(function ($) {
-	'use strict';
+    'use strict';
 
-	let initSidebar = function () {
-		let btnCall = $('#call-sidebar'),
-			overlay = $('#cpanel-overlay'),
-			templateCpanel = $('#template-cpanel');
+    let initSidebar = function () {
+        let btnCall = $('#call-sidebar'),
+            overlay = $('#cpanel-overlay'),
+            templateCpanel = $('#template-cpanel');
 
-		btnCall.add(overlay).click(function () {
-			if (!templateCpanel.is('.sidebar-show')) {
-				templateCpanel.addClass('sidebar-show').attr({'style': 'overflow: hidden; height: 100vh; position: fixed'});
-			} else {
-				templateCpanel.removeClass('sidebar-show').attr({'style': ''});
-			}
-		});
-	}
+        btnCall.add(overlay).click(function () {
+            if (!templateCpanel.is('.sidebar-show')) {
+                templateCpanel.addClass('sidebar-show').attr({'style': 'overflow: hidden; height: 100vh; position: fixed'});
+            } else {
+                templateCpanel.removeClass('sidebar-show').attr({'style': ''});
+            }
+        });
+    }
 
-	let initClipboardCopy = function (value) {
-		let createTextarea = document.createElement('textarea');
-		createTextarea.style.cssText = 'position: absolute; left: -99999px';
-		createTextarea.setAttribute("id", "textareaCopy");
-		document.body.appendChild(createTextarea);
-		let textareaElm = document.getElementById('textareaCopy');
-		textareaElm.value = value;
-		textareaElm.select();
-		textareaElm.setSelectionRange(0, 99999);
-		document.execCommand("copy");
-		textareaElm.remove();
-	}
+    let initClipboardCopy = function (value) {
+        let createTextarea = document.createElement('textarea');
+        createTextarea.style.cssText = 'position: absolute; left: -99999px';
+        createTextarea.setAttribute("id", "textareaCopy");
+        document.body.appendChild(createTextarea);
+        let textareaElm = document.getElementById('textareaCopy');
+        textareaElm.value = value;
+        textareaElm.select();
+        textareaElm.setSelectionRange(0, 99999);
+        document.execCommand("copy");
+        textareaElm.remove();
+    }
 
-	let initCheckAll = function () {
-		$('.check-all').click(function () {
-			if ($(".check-only").is("checked")) {
-				$('.check-only').attr('checked', false);
-			} else {
-				$('.check-only').attr('checked', true);
-			}
-		});
-	}
+    let initCheckAll = function () {
+        $('.check-all').click(function () {
+            if ($(".check-only").is("checked")) {
+                $('.check-only').attr('checked', false);
+            } else {
+                $('.check-only').attr('checked', true);
+            }
+        });
+    }
 
-	$(function () {
-		initSidebar();
-		initCheckAll();
-		$(document).on('click', '.copy-value', function () {
-			if ($(this).attr('data-value') != undefined) {
-				initClipboardCopy($(this).attr('data-value'));
-			} else {
-				initClipboardCopy($(this).parent().find('input').val());
-			}
-		});
-	});
+    let initSelect2 = function () {
+        if ($('.initSelect2').length) {
+            $('.initSelect2').select2();
+        }
+    }
+
+    $(function () {
+        initSidebar();
+        initCheckAll();
+        initSelect2();
+
+
+        $(document).on('click', '.copy-value', function () {
+            if ($(this).attr('data-value') != undefined) {
+                initClipboardCopy($(this).attr('data-value'));
+            } else {
+                initClipboardCopy($(this).parent().find('input').val());
+            }
+        });
+    });
 })(jQuery);
